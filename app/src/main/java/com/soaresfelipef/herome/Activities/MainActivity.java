2a013c368fc4a80b4a912c43d00bb90872bcc8b9
Felipe Soares <soaresfelipef@gmail.com>
2016-09-14 16:29:15 -0300

    first screen done

--- app/src/main/java/com/soaresfelipef/herome/Activities/MainActivity.java
+++ app/src/main/java/com/soaresfelipef/herome/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.soaresfelipef.herome;
+package com.soaresfelipef.herome.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.soaresfelipef.herome.R;
 
 public class MainActivity extends AppCompatActivity {
 
